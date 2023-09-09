6a4e2f41170436a7b4293dd56e20c7f9b643bf6e
paranjaa <paranjaa@uregina.ca>
2023-09-08 22:12:28 -0600

    Added header + intro text, tested multiple files

---
+++ input.cpp
@@ -0,0 +1,13 @@
+/**
+  Project: Assignment #1
+  Course: ENSE 452
+  Date: Sept 8th, 2023
+  Programmer: Alok Paranjape
+  Description: Making a complex-calculator with a command line
+  this file is for taking user input
+*/
+
+int add(int x, int y)
+{
+    return x + y;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
+/**
+  Project: Assignment #1
+  Course: ENSE 452
+  Date: Sept 8th, 2023
+  Programmer: Alok Paranjape
+  Description: Making a complex-calculator with a command line
+*/
+
 #include <iostream>
+#include "input.cpp"
 using namespace std;
 
+int add(int x, int y);
+
+
 int main(){
-    cout << "Hello World, this is the main file" << endl;
-    cout << "Enter exp:";
+    //cout << "Hello World, this is the main file" << endl;
+    cout << "Complex calculator" << endl;
+    cout << "Type a letter to specify the arithmetic operator (A, S, M, D)" << endl;
+    cout << "followed by two complex numbers expressed as pairs of doubles." << endl;
+    cout << "Type Q to quit" << endl;
+    //cout << "Enter exp:";
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
+
     return 0;
 }
