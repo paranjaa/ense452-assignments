79ed172c699700eb94f8eee55917aa947a7df0e6
paranjaa <paranjaa@uregina.ca>
2023-09-08 20:30:32 -0600

    Initial commit, hoping this works

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    cout << "Testing if Git works" << endl;
+    cout << "(possibly Github after that)" << endl;
+    return 0;
+}
