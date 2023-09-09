999fb7d2e7f7965f2efde356589ec20996b4f4f0
paranjaa <paranjaa@uregina.ca>
2023-09-08 21:00:23 -0600

    third time's the charm

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     cout << "Hello World" << endl;
-    cout << "Testing if Git works (made some changes/updates)" << endl;
+    cout << "Didn't work again, might have had wrong URL" << endl;
     cout << "(possibly Github after that)" << endl;
     return 0;
 }
