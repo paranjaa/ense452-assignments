ee01c8d45047af8cf200a8fc66fc8e822f83b752
paranjaa <paranjaa@uregina.ca>
2023-09-11 18:49:43 -0600

    Fixed not calling getSign (was printing as 'e')

--- calc.cpp
+++ calc.cpp
@@ -11,19 +11,20 @@ using namespace std;
 
 char getSign(double result_imaginary)
 {
-        if(result_imaginary < 0)
-        {
-            return '-';
-        }
-        else
-        {
-            return '+';
-        }
+    if(result_imaginary < 0)
+    {
+        return '-';
+    }
+    else
+    {
+        return '+';
+    }
 
 }
 
 void printResult(double result_real, double result_imaginary, char result_sign)
 {
+    cout << endl;
     cout << result_real;
     cout << " " << result_sign << " j ";
     cout << abs(result_imaginary) << endl;
@@ -33,7 +34,7 @@ void calcAdd(Complex operand1, Complex operand2)
 {
     double result_real = (operand1.real + operand2.real);
     double result_imaginary = (operand1.imaginary + operand2.imaginary);
-    char result_sign = (result_imaginary);
+    char result_sign = getSign(result_imaginary);
     printResult(result_real, result_imaginary, result_sign);
     
 }
