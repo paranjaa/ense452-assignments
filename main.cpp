b28093b4d645645ccd076523b3c559e2a9d3546b
paranjaa <paranjaa@uregina.ca>
2023-09-19 20:33:01 -0600

    Trying to work on comp division

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 */
 
 #include <iostream>
+#include<bits/stdc++.h>
 #include "input.h"
 //#include "complex.h"
 using namespace std;
@@ -17,19 +18,42 @@ int main(){
     // cout << "Complex calculator" << endl;
     // cout << "Type a letter to specify the arithmetic operator (A, S, M, D)" << endl;
     // cout << "followed by two complex numbers expressed as pairs of doubles." << endl;
+    
     // cout << "Type Q to quit" << endl;
+    // string input = "a 1 2 3 1 213221 321";
+    // int result = countWords(input);
+    // cout << "There are " << result;
+    // cout << " words in the test sentence" << endl;
+    // if(result < 5)
+    // {
+    //   cout << "error: too few arguements" << endl;
+    // }
+    // if(result > 5)
+    // {
+    //   cout << "error: too many arguements" << endl;
+    // }
+    // return 0;
+
     cout << "Hello World" << endl;
-    string input = "a 1 2 3 1 213221 321";
-    int result = countWords(input);
-    cout << "There are " << result;
-    cout << " words in the test sentence" << endl;
-    if(result < 5)
-    {
-      cout << "error: too few arguements" << endl;
-    }
-    if(result > 5)
-    {
-      cout << "error: too many arguements" << endl;
-    }
-    return 0;
+
+    double x1 = 1.1;
+    double i1 = 2.0;
+
+   
+
+    double m1 = (sqrt(x1*x1 + i1 * i1));
+    double a1 = 0;
+
+
+    double x2 = 3.0;
+    double i2 = 4.0;
+
+    double m2 = (sqrt(x2*x2 + i2 * i2));
+    double a2;
+
+    double x3 = 0.0;
+    double i3 = 0.0;
+
+    cout << "The answer is: " << x3 << " j " << i3 << endl;
+   
 }
