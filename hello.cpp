df92de1014e15ea57c556467c926d9342dee380f
paranjaa <paranjaa@uregina.ca>
2023-09-21 22:58:45 -0600

    Removed since it wasn't needed

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "Hello World" << endl;
-    cout << "(think I got it to work)" << endl;
-    cout << "(possibly Github after that)" << endl;
-    return 0;
-}
