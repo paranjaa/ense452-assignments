7663a7ac5ddbe50a08cb5b6b2c4023817022c04b
paranjaa <paranjaa@uregina.ca>
2023-10-15 20:17:44 -0600

    Added in code for handling inserting at head/other positions, still needs testing

--- assignment2/queue.cpp
+++ assignment2/queue.cpp
@@ -22,9 +22,9 @@ Queue::~Queue()
 {
     for (QElement* qe = head; qe != 0;)
     {
-	QElement* temp = qe;
-	qe = qe->next;
-	delete(temp);
+        QElement* temp = qe;
+        qe = qe->next;
+        delete(temp);
     }
 }
 
@@ -59,6 +59,44 @@ void Queue::insert(Data d)
     }
     tail = el;
     nelements++;
+}
+
+void Queue::insert(Data d, unsigned position)
+{
+    cout << "Hello World, inserting an element at a position" << endl;
+    QElement* new_element = new QElement(d);
+
+    //if the given position is after the tail's next space, then end it there
+    // if(position > (size() + 1))
+    // {
+    //     cout << "Error" << endl;
+    //     return;
+    // }
+
+    if(position == 1)
+    {
+        cout << "adding a new element at the head position" << endl;
+        new_element->next = head;
+        head = new_element;
+        nelements++;
+        return;
+    }
+
+    QElement* prev_element = head;
+    unsigned position_copy = position;
+    while(--position_copy > 1)
+    {
+        prev_element = prev_element ->next;
+    }
+    cout << "the position minus 1th element is" << endl;
+    cout << "(" << prev_element->data.x << "," << prev_element->data.y << ") " << endl;
+    new_element->next = prev_element->next;
+    prev_element->next = new_element;
+    nelements++;
+
+
+
+
 }
 
 bool Queue::search(Data otherData) const
--- assignment2/testq.cpp
+++ assignment2/testq.cpp
@@ -46,33 +46,41 @@ int main()
 
     q1.print();
 
-    vector<Data> dataVec;
-    dataVec.push_back(Data(1,1));
-    dataVec.push_back(Data(3,3));
-    dataVec.push_back(Data(5,5));
+    q1.insert(Data(2,2), 1);
 
-    assert(testQueueEquality(q1, dataVec));
+    q1.print();
 
-    Data d44(4, 4);
-    bool found = q1.search(d44);
-    assert(found == false);
+    q1.insert(Data(3,3), 2);
+
+    q1.print();
+
+    // vector<Data> dataVec;
+    // dataVec.push_back(Data(1,1));
+    // dataVec.push_back(Data(3,3));
+    // dataVec.push_back(Data(5,5));
+
+    // assert(testQueueEquality(q1, dataVec));
+
+    // Data d44(4, 4);
+    // bool found = q1.search(d44);
+    // assert(found == false);
 
 
-    q1.insert(d44);  // now is (1,1),(3,3),(5,5),(4,4)
-    found = q1.search(d44);
-    assert(found == true);
+    // q1.insert(d44);  // now is (1,1),(3,3),(5,5),(4,4)
+    // found = q1.search(d44);
+    // assert(found == true);
 
-    // now queue is(1,1),(3,3),(5,5),(4,4) and 
-    // dataVec has (1,1),(3,3),(5,5).  Not equal
-    assert(testQueueEquality(q1, dataVec) == false);
+    // // now queue is(1,1),(3,3),(5,5),(4,4) and 
+    // // dataVec has (1,1),(3,3),(5,5).  Not equal
+    // assert(testQueueEquality(q1, dataVec) == false);
 
-    Data temp;
-    q1.remove(&temp);  // now q1 is (3,3),(5,5),(4,4)
+    // Data temp;
+    // q1.remove(&temp);  // now q1 is (3,3),(5,5),(4,4)
 
-    Data temp2(1,1);
-    assert(temp.equals(temp2));  // (1,1) == (1,1)
+    // Data temp2(1,1);
+    // assert(temp.equals(temp2));  // (1,1) == (1,1)
 
-    Data temp3(6,6);
-    found = q1.search(temp3);
-    assert(found == false);
+    // Data temp3(6,6);
+    // found = q1.search(temp3);
+    // assert(found == false);
 }
